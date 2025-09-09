9755a2c868c79cd5c185d18df3752ff87cfa8e20
chandragupt074 <156681191+chandragupt074@users.noreply.github.com>
2025-09-09 10:55:51 +0530

    Update video.route.js

--- routes/video.route.js
+++ routes/video.route.js
@@ -4,9 +4,9 @@ import isAuthenticated from "../middlewares/isAuthenticated.js";
 const router = express.Router();
 
 
-router.route("/video").post(createVideo)
-router.route("/video").get(getAllVideos)
-router.route("/video/:id").get(getSingleVideo)
+router.route("/video").post(isAuthenticated,createVideo)
+router.route("/video").get(isAuthenticated,getAllVideos)
+router.route("/video/:id").get(isAuthenticated,getSingleVideo)
 router.route("/video/:id").put(isAuthenticated,updateVideo)
 router.route("/video/:id").delete(isAuthenticated,deleteVideo)
 
